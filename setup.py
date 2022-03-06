8540ae361d22bfc159fa3d85adad07a49080fffa
Richard Scott <Richard.Scott@ozminerals.com>
2022-03-06 10:51:54 +1030

    fix organic powers typo

---
+++ setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+with open("requirements.txt", "r") as fh:
+    requirements = [line.strip() for line in fh]
+
+setuptools.setup(
+    name="is-number",
+    version="0.0.1",
+    author="Super Squadron",
+    author_email="super squadron@supersquadron.org",
+    description="Utilities for the Supersquadron game",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    install_requires=requirements,
+)
