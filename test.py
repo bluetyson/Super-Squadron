1abf1f0ac16c85dfdc4f44291987850158b88f06
Richard Scott <Richard.Scott@ozminerals.com>
2022-03-14 10:45:56 +1030

    Elasticity

--- test.py
+++ test.py
@@ -49,80 +49,87 @@ if __name__ == '__main__':
     ad = dict2[powername](Character)
 
     powername = 'Armour'
-    Character['Powers']['Number'] = 6
+    Character['Powers']['Number'] = 7
     Character['Powers']['List'].append(powername)
     Character['Powers']['Detail'][powername] = {}
     dict2[powername] = powers.Armour
     ad = dict2[powername](Character)
 
     powername = 'Astral Projection'
-    Character['Powers']['Number'] = 7
+    Character['Powers']['Number'] = 8
     Character['Powers']['List'].append(powername)
     Character['Powers']['Detail'][powername] = {}
     dict2[powername] = powers.AstralProjection
     ad = dict2[powername](Character)
 
     powername = 'Body Augmentation'
-    Character['Powers']['Number'] = 8
+    Character['Powers']['Number'] = 9
     Character['Powers']['List'].append(powername)
     Character['Powers']['Detail'][powername] = {}
     dict2[powername] = powers.BodyAugmentation
     ad = dict2[powername](Character)
 
     powername = 'Cybernetics'
-    Character['Powers']['Number'] = 9
+    Character['Powers']['Number'] = 10
     Character['Powers']['List'].append(powername)
     Character['Powers']['Detail'][powername] = {}
     dict2[powername] = powers.Cybernetics
     ad = dict2[powername](Character)
 
     powername = 'Darkness Generation'
-    Character['Powers']['Number'] = 10
+    Character['Powers']['Number'] = 11
     Character['Powers']['List'].append(powername)
     Character['Powers']['Detail'][powername] = {}
     dict2[powername] = powers.DarknessGeneration
     ad = dict2[powername](Character)
 
     powername = 'Death Touch'
-    Character['Powers']['Number'] = 11
+    Character['Powers']['Number'] = 12
     Character['Powers']['List'].append(powername)
     Character['Powers']['Detail'][powername] = {}
     dict2[powername] = powers.DeathTouch
     ad = dict2[powername](Character)
 
     powername = 'Defect'
-    Character['Powers']['Number'] = 12
+    Character['Powers']['Number'] = 13
     Character['Powers']['List'].append(powername)
     Character['Powers']['Detail'][powername] = {}
     dict2[powername] = powers.Defect
     ad = dict2[powername](Character)
 
     powername = 'Density Control'
-    Character['Powers']['Number'] = 13
+    Character['Powers']['Number'] = 14
     Character['Powers']['List'].append(powername)
     Character['Powers']['Detail'][powername] = {}
     dict2[powername] = powers.DensityControl
     ad = dict2[powername](Character)
 
     powername = 'Dimensional Gate'
-    Character['Powers']['Number'] = 14
+    Character['Powers']['Number'] = 15
     Character['Powers']['List'].append(powername)
     Character['Powers']['Detail'][powername] = {}
     dict2[powername] = powers.DimensionalGate
     ad = dict2[powername](Character)
 
     powername = 'Disintegration Beam'
-    Character['Powers']['Number'] = 15
+    Character['Powers']['Number'] = 16
     Character['Powers']['List'].append(powername)
     Character['Powers']['Detail'][powername] = {}
     dict2[powername] = powers.DisintegrationBeam
     ad = dict2[powername](Character)
 
     powername = 'Ego Change'
-    Character['Powers']['Number'] = 16
+    Character['Powers']['Number'] = 17
     Character['Powers']['List'].append(powername)
     Character['Powers']['Detail'][powername] = {}
     dict2[powername] = powers.EgoChange
+    ad = dict2[powername](Character)
+
+    powername = 'Elasticity'
+    Character['Powers']['Number'] = 18
+    Character['Powers']['List'].append(powername)
+    Character['Powers']['Detail'][powername] = {}
+    dict2[powername] = powers.Elasticity
     ad = dict2[powername](Character)
 
     print(Character)
